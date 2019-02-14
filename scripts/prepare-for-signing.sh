6eeff4ddd0feae974143108b774eba21ed5c4415
Chris Lormor <clormor@palantir.com>
2019-02-14 17:19:43 +0000

    sign in circle i hope

--- scripts/prepare-for-signing.sh
+++ scripts/prepare-for-signing.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "preparing key file... $MAVEN_SIGN_KEY_FILE"
-echo $MAVEN_SIGN_SECRET | base64 -d > $MAVEN_SIGN_KEY_FILE
+echo $MAVEN_SIGN_SECRET | base64 --decode > $MAVEN_SIGN_KEY_FILE
 
